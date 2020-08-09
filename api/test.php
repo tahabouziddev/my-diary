ee53b2c38c36d9a3d592c0aba077b05230b7d850
= <=>
2020-08-09 15:30:46 +0100

    test

--- api/test.php
+++ api/test.php
@@ -1,24 +1,33 @@
 <?php
+// if(isset($_GET) && $_GET["doit"]="hi" ){
 
 
 
-
-$link = mysqli_connect("shareddb-u.hosting.stackcp.net", "secret-diary-3133337529", "48a0gc2zih", "secret-diary-3133337529");
+// $link = mysqli_connect("shareddb-u.hosting.stackcp.net", "secret-diary-3133337529", "48a0gc2zih", "secret-diary-3133337529");
         
-if (mysqli_connect_error()) {
+// if (mysqli_connect_error()) {
     
-    die ("Database Connection Error");
+//     die ("Database Connection Error");
     
-}
-$i=1;
-$diarynumber=strval($i);
-$diary= "diary"+ $diarynumber;
-while($i<365){
+// }
+// $i=1;
+// $diarynumber=strval($i);
+// $f="diary";
+// $diary= $f . $diarynumber;
 
-    mysqli_query( $link,"ALTER TABLE users ADD $diary VARCHAR(50) NOT NULL");
-$i=$i+1;
 
-}
+// while($i<366){
+
+//     mysqli_query( $link,"ALTER TABLE `diarycontent`ADD $diary TEXT NOT NULL");
+//     // echo $d;
+//     echo $diary;
+
+// $i=$i+1;
+// $diarynumber=strval($i);
+// $diary= $f . $diarynumber;
+
+// // http://makemoneyy.tech/test.php?doit=hi
+// }
 
 
 
